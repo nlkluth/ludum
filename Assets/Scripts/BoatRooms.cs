087f740889e29bb7549f928bb4ac2a4ce29f7265
Nathan Kluth <nlkluth@gmail.com>
2014-08-23 16:24:17 -0600

    making pseudo bounds

--- Assets/Scripts/BoatRooms.cs
+++ Assets/Scripts/BoatRooms.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class PseudoBounds
+{
+	public float xMax;
+	public float yMax;
+}
+
 public class BoatRooms : MonoBehaviour 
 {
 	private GameObject[] _crewMembers;
-	
+	private Transform _transform;
+	public PseudoBounds bounds;
+
 	void Start()
-	{
+	{	_transform = gameObject.GetComponent<Transform>();
 		_crewMembers = GameObject.FindGameObjectsWithTag("CrewMember");
 	}
 	
@@ -22,6 +31,12 @@ public class BoatRooms : MonoBehaviour
 	{
 		Camera OverlayCamera = GameObject.FindGameObjectWithTag("OverlayCamera").GetComponent<Camera>();
 		var mousePosition = OverlayCamera.ScreenToWorldPoint(Input.mousePosition);
+
+		if (mousePosition.x > _transform.position.x + bounds.xMax || mousePosition.x < _transform.position.x - bounds.xMax ||
+			mousePosition.y > _transform.position.y + bounds.yMax || mousePosition.y < _transform.position.y - bounds.yMax)
+		{
+			return;	
+		}
 		
 		foreach (GameObject crewmember in _crewMembers)
 		{
