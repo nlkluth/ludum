7243690e7129da2d1367a5349475280a933e551d
Nathan Kluth <nlkluth@gmail.com>
2014-08-23 10:36:08 -0600

    set crewmember up to handle selection

--- Assets/Scripts/CrewMember.cs
+++ Assets/Scripts/CrewMember.cs
@@ -3,14 +3,22 @@ using System.Collections;
 
 public class CrewMember : MonoBehaviour 
 {
+	public bool isSelected;
+
 	void OnMouseDown()
 	{
-		var mousePosition = Input.mousePosition;
 		showSelect();
 	}
 
 	private void showSelect()
 	{
+		isSelected = true;
 		Debug.Log ("add selected look");
+	}
+
+	public void MoveToMousePosition(Vector2 mousePosition)
+	{
+		Debug.Log ("clicked");
+		Debug.Log (mousePosition);
 	}
 }
