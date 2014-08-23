e21d303f81fe093203d4e86a62f386030cbc8e30
Nathan Kluth <nlkluth@gmail.com>
2014-08-22 23:48:09 -0600

    select the crew member

---
+++ Assets/Scripts/CrewMember.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrewMember : MonoBehaviour 
+{
+	private bool _isSelected = false;
+
+	void OnMouseDown()
+	{
+		var mousePosition = Input.mousePosition;
+		_isSelected = true;
+	}
+}
