7cd6598445a7a9358a559aa169191ffc9b6850b7
Nathan Kluth <nlkluth@gmail.com>
2014-08-22 21:19:56 -0600

    initial script setup for overlay

---
+++ Assets/Scripts/Overlay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Overlay : MonoBehaviour {
+	private bool open = false;
+
+	void OnMouseDown()
+	{
+		openOrCloseOverlay();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.F)) 
+		{
+			openOrCloseOverlay();
+		}
+	}
+
+	private void openOrCloseOverlay()
+	{
+		open = !open;
+
+		if (open)
+		{
+			return;
+		}
+	}
+}
