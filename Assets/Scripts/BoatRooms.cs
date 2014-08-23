68ab24da4a23815668b6bf8befd3222b1a7376aa
Nathan Kluth <nlkluth@gmail.com>
2014-08-23 10:35:33 -0600

    added boat room script

---
+++ Assets/Scripts/BoatRooms.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoatRooms : MonoBehaviour 
+{
+	private GameObject[] _crewMembers;
+
+	void Start()
+	{
+		_crewMembers = GameObject.FindGameObjectsWithTag("crewMember");
+	}
+
+	void OnMouseDown()
+	{
+		var mousePosition = Input.mousePosition; 
+		Debug.Log ("click!");
+
+		foreach (GameObject crewmember in _crewMembers)
+		{
+			var current = crewmember.GetComponent<CrewMember>();
+			if (current.isSelected)
+			{
+				current.MoveToMousePosition(mousePosition);
+			}
+		}
+	}
+}
