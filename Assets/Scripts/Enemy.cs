a824e20cce978f7329fb5846147f4e08d4616950
Nathan Kluth <nlkluth@gmail.com>
2014-08-24 00:11:13 -0600

    setting enemy movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,34 +3,41 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
-	public float moveSpeed;
+	public float speed;
 	public float rotationSpeed;
 	public float maxDistance;
 	
 	private Transform _target;
 	private Transform localTransform;
+	private bool _cruising = true;
 	
 	void Awake()
 	{
 		localTransform = transform;
 	}
-	
+
 	void OnTriggerEnter2D(Collider2D collider)
 	{
 		if (collider.tag == "Player")
 		{
 			_target = collider.transform;
-			Debug.Log (_target);
+			_cruising = false;
 		}
 	}
 	
-	//	void Update () 
-	//	{
-	//		localTransform.rotation = Quaternion.Slerp(localTransform.rotation, Quaternion.LookRotation(_target.position - localTransform.position), rotationSpeed * Time.deltaTime);
-	//		
-	//		if (Vector3.Distance(_target.position, localTransform.position) > maxDistance)
-	//		{
-	//			localTransform.position += localTransform.forward * moveSpeed * Time.deltaTime;
-	//		}
-	//	}
+	void Update () 
+	{
+		if (_cruising)
+		{
+			transform.Translate(Vector3.right * Time.deltaTime * speed);
+			return;
+		}
+
+		localTransform.rotation = Quaternion.Slerp(localTransform.rotation, Quaternion.LookRotation(_target.position - localTransform.position), rotationSpeed * Time.deltaTime);
+		
+		if (Vector3.Distance(_target.position, localTransform.position) > maxDistance)
+		{
+			localTransform.position += localTransform.forward * speed * Time.deltaTime;
+		}
+	}
 }
