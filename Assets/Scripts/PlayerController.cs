fc57ae45b90fa56bff0c7bb5eb3326fa26310b17
Jordan Buehler <Jordo.buehler@gmail.com>
2014-08-24 14:15:23 -0500

    Created Player Controller script
    The player object is now able to move up, down, left, and right while
    staying in the boundary box. The speed of the ship and the boundary box
    can be adjusted as needed.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, yMin, yMax;
+
+}
+
+public class PlayerController : MonoBehaviour 
+{
+
+	public float speed;
+	///public float tilt;
+	public Boundary boundary;
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+		rigidbody.velocity = movement * speed;
+
+		rigidbody.position = new Vector3
+		(
+			Mathf.Clamp (rigidbody.position.x, boundary.xMin, boundary.xMax),
+			Mathf.Clamp (rigidbody.position.y, boundary.yMin, boundary.yMax),
+			0.0f
+		);
+
+		///rigidbody.rotation = Quaternion.Euler (0.0f, rigidbody.velocity.y * -tilt, 0.0f);
+	}
+}
