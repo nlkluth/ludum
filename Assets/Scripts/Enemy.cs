9627d1b436ec730b1557dcc05e5debbb93f76d78
Nathan Kluth <nlkluth@gmail.com>
2014-08-23 21:47:47 -0600

    working on enemy targetting

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float moveSpeed;
+	public float rotationSpeed;
+	public float maxDistance;
+	
+	private Transform _target;
+	private Transform localTransform;
+	
+	void Awake()
+	{
+		localTransform = transform;
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.tag == "Player")
+		{
+			_target = collider.transform;
+			Debug.Log (_target);
+		}
+	}
+	
+	//	void Update () 
+	//	{
+	//		localTransform.rotation = Quaternion.Slerp(localTransform.rotation, Quaternion.LookRotation(_target.position - localTransform.position), rotationSpeed * Time.deltaTime);
+	//		
+	//		if (Vector3.Distance(_target.position, localTransform.position) > maxDistance)
+	//		{
+	//			localTransform.position += localTransform.forward * moveSpeed * Time.deltaTime;
+	//		}
+	//	}
+}
