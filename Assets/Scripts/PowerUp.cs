8b1b0ef03836285983400316bae4275348342913
Nathan Kluth <nlkluth@gmail.com>
2014-08-25 00:23:12 -0600

    adding powerup class

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class PowerUpType
+{
+
+}
+
+public class PowerUp : MonoBehaviour
+{
+	public PowerUpType powerUp;
+
+	private GameObject player;
+	private PlayerInventory playerInventory;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerInventory = player.GetComponent<PlayerInventory>();
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.tag == "Player")
+		{
+			playerInventory.AddPowerUp(this.powerUp);
+			Destroy(gameObject);
+		}
+	}
+}
