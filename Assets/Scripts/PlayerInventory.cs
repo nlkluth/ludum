7200ad74a3cda97721fd69c291a354a6485aac1a
Nathan Kluth <nlkluth@gmail.com>
2014-08-25 00:23:28 -0600

    stubbing player inventory script

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInventory : MonoBehaviour 
+{
+	public void AddPowerUp(PowerUpType powerUp)
+	{
+		Debug.Log ("Got powerup");
+	}
+}
