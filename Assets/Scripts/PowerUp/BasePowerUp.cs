e700000527e1740cd277ac15f5fbc924f8cd3dac
Nathan Kluth <nlkluth@gmail.com>
2014-08-25 08:35:02 -0600

    adding base powerup class

---
+++ Assets/Scripts/PowerUp/BasePowerUp.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasePowerUp
+{
+	public string Name { get; set; }
+	public int ActionPoints { get; set; }
+	public PowerUpType PowerUpType { get; set; }
+
+	public BasePowerUp()
+	{
+		Name = "";
+		ActionPoints = 1;
+		PowerUpType = PowerUpType.None;
+	}
+}
+
+public enum PowerUpType
+{
+	Weapon,
+	Armor,
+	Special,
+	None
+}
