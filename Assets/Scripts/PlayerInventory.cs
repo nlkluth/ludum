379c33523f5031ba4b394b3c30080c70218e86d7
Nathan Kluth <nlkluth@gmail.com>
2014-08-27 08:28:04 -0600

    comment out missing references for now

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 public class PlayerInventory : MonoBehaviour
 {
-	public Player player;
+//	public Player player;
 	public int SpecialCount { get; set; }
 
 	public void AddPowerUp(PowerUpType powerUp)
 	{
 		if (powerUp == PowerUpType.Special)
     		{
-		      	return SpecialCount++;
+		      	SpecialCount++;
+			return;
 		}
-
-    		player.IncreaseStat(powerUp);
+		return;
+//    		player.IncreaseStat(powerUp);
 	}
 }
