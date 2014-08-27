ed0ec01d20663e6151213dab1024664bc608b564
Nathan Kluth <nlkluth@gmail.com>
2014-08-27 08:28:14 -0600

    moved power up script

--- Assets/Scripts/PowerUp/PowerUp.cs
+++ Assets/Scripts/PowerUp/PowerUp.cs
@@ -4,16 +4,16 @@ using System.Collections;
 public class PowerUp : MonoBehaviour
 {
 	public PowerUpType PowerUpType { get; set; }
-
+	
 	private GameObject player;
 	private PlayerInventory playerInventory;
-
+	
 	void Awake()
 	{
 		player = GameObject.FindGameObjectWithTag("Player");
 		playerInventory = player.GetComponent<PlayerInventory>();
 	}
-
+	
 	void OnTriggerEnter2D(Collider2D collider)
 	{
 		if (collider.tag == "Player")
@@ -22,4 +22,14 @@ public class PowerUp : MonoBehaviour
 			Destroy(gameObject);
 		}
 	}
+}
+
+[System.Serializable]
+public enum PowerUpType
+{
+	Weapon,
+	Armor,
+	Special,
+	Speed,
+	None
 }
