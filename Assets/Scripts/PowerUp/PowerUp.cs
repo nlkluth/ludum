029fcfababc086b647ba6227f3d673932259513c
Nathan Kluth <nlkluth@gmail.com>
2014-09-01 14:55:31 -0600

    added speed (and default) and localtransform reference

--- Assets/Scripts/PowerUp/PowerUp.cs
+++ Assets/Scripts/PowerUp/PowerUp.cs
@@ -4,14 +4,19 @@ using System.Collections;
 public class PowerUp : MonoBehaviour
 {
 	public PowerUpType powerUpType;
+	public float speed = 1;
 
 	private GameObject player;
 	private PlayerInventory playerInventory;
+	private Transform localTransform;
 	
 	void Awake()
 	{
 		player = GameObject.FindGameObjectWithTag("Player");
 		playerInventory = player.GetComponent<PlayerInventory>();
+		localTransform = transform;
+	}
+
 	}
 	
 	void OnTriggerEnter2D(Collider2D collider)
