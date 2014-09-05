f448246938cde161f84ce0636f0aba6281b9b637
Nathan Kluth <nlkluth@gmail.com>
2014-09-05 09:19:59 -0600

    created game controller for spawning enemies

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject Enemy;
+	public Vector3 sawnValues; 
+
+	public float enemyStartWait; 
+	public float enemySpawnWait; 
+	public float enemyWaveCount; 
+	public float enemyWaveWait;
+
+	void Start()
+	{
+		StartCoroutine(SpawnEnemies());
+	}
+	
+	IEnumerator SpawnEnemies()
+	{
+		yield return new WaitForSeconds(enemyStartWait);
+
+		while (true)
+		{
+			for (int i = 0; i < enemyWaveCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3(Random.Range());
+				Quaternion spawnRotation = Quaternion.identity;
+				yield return new WaitForSeconds(enemySpawnWait);
+			}
+
+			yield return new WaitForSeconds(enemyWaveWait);
+		}
+	}
+}
