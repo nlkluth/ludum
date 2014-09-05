c9b187742e06b02d12eea4091ee2f1400d22f871
Nathan Kluth <nlkluth@gmail.com>
2014-09-05 09:44:33 -0600

    running into an enemy ship's body destroy the enemy + the player

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour 
+{
+	void OnTriggerEnter2D(Collider2D collider) 
+	{
+		if (collider.tag == "Player")
+		{
+			Destroy(collider.gameObject);
+			Destroy(transform.parent.gameObject);
+		}
+	}
+}
