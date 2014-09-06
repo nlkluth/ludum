b11aebfec46c5f3ce6055cd71aa07ef11a2781c7
Nathan Kluth <nlkluth@gmail.com>
2014-09-06 11:56:43 -0600

    created cannonball script

---
+++ Assets/Scripts/Cannonball.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannonball : MonoBehaviour 
+{
+	public float speed;
+	public Vector3 Direction { get; set; }
+
+	void Start()
+	{
+		rigidbody2D.velocity = Direction;
+	}
+}
