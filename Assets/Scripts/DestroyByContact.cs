5416c76873de42b7cad45f79fc495e6395121ac4
Nathan Kluth <nlkluth@gmail.com>
2014-09-08 17:49:59 -0600

    updated destory by contact on enemy body to take damage if it's a cannonball

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,12 +3,25 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour 
 {
+	public Enemy enemy;
+
+	void Awake()
+	{
+		enemy = transform.parent.gameObject.GetComponent<Enemy>();
+	}
+
 	void OnTriggerEnter2D(Collider2D collider) 
 	{
 		if (collider.tag == "Player")
 		{
 			Destroy(collider.gameObject);
 			Destroy(transform.parent.gameObject);
+		}
+
+		if (collider.tag == "Cannonball")
+		{
+			Destroy(collider.gameObject);
+			enemy.TakeDamage();
 		}
 	}
 }
