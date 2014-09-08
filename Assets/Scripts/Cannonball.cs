ac73586db0fc9194b9a907ba6b1029193116b747
Nathan Kluth <nlkluth@gmail.com>
2014-09-08 17:49:41 -0600

    cannonball script removed for now

--- Assets/Scripts/Cannonball.cs
+++ Assets/Scripts/Cannonball.cs
@@ -3,11 +3,5 @@ using System.Collections;
 
 public class Cannonball : MonoBehaviour 
 {
-	public float speed;
-	public Vector3 Direction { get; set; }
 
-	void Start()
-	{
-		rigidbody2D.velocity = Direction;
-	}
 }
