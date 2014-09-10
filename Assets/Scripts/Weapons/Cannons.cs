ebc63061fe9c8209a52110476a7ff526da5f7b39
Nathan Kluth <nlkluth@gmail.com>
2014-09-10 08:41:01 -0600

    cannons will be responsible for shooting cannonballs

---
+++ Assets/Scripts/Weapons/Cannons.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannons : MonoBehaviour 
+{
+	private Transform[] spawnPositions;
+	private int shotCount = 0;
+
+	public void CreateCannonball()
+	{
+		shotCount++;
+		if (shotCount > 5)
+		{
+			shotCount = 0;
+		}
+		
+		Instantiate(gameObject, spawnPositions[shotCount].position, spawnPositions[shotCount].rotation);
+	}
+}
